7391b39bd3687c144e1a35d3c3a0ade9d269386f
amachkel <amachkel@gmail.com>
2022-05-06 12:24:15 -0400

    can add friends. still adding empty friends array to existing friends array.

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -69,13 +69,15 @@ module.exports = {
 
   // Add a friend to a user
   addFriend(req, res) {
-    console.log("You are adding a friend");
+    // console.log("You are adding a friend");
     console.log(req.body);
     User.findOneAndUpdate(
       { _id: req.params.userId },
       { $addToSet: { friends: req.body } },
       { runValidators: true, new: true }
     )
+      .populate({ path: "friends", select: "-__v" })
+      .select("-__v")
       .then((user) =>
         !user
           ? res.status(404).json({ message: "No user found with that ID" })
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -12,11 +12,11 @@ const {
 
 router.route("/").get(getUsers).post(createUser);
 
-router
-  .route("/:userId")
-  .get(getSingleUser)
-  .put(updateUser)
-  .delete(deleteUser)
-  .post(addFriend)
-  .delete(removeFriend);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// /api/user/:userId/friends/
+router.route("/:userId/friends").post(addFriend);
+
+router.route("/:userId/friends/:friendId").delete(removeFriend);
+
 module.exports = router;
